e1a0449910b6735602139af3b777c90abf1677e9
Viraj-Sankalpa <virajsankalpa@gmail.com>
2023-10-03 22:18:26 +0530

    created a ToDo container

---
+++ src/containers/ToDo.js
@@ -0,0 +1,18 @@
+const ToDo = () => {
+    return ( 
+        <div>
+            <h1>TODO</h1>
+        <h2>ADD YOUR TASK</h2>
+        <input/>
+        <button>ADD</button>
+
+        <h2>Today's Task List</h2>
+
+        <ul>
+            <li>Task 1</li>
+        </ul>
+        </div>
+     );
+}
+ 
+export default ToDo;
